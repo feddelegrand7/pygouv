075840d002b5921b5fa8d4fc55452c99fe95c237
feddelegrand7 <ihaddaden.fodeil@gmail.com>
2020-12-19 23:54:08 +0100

    incrementing version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 setup(
 
     name='pygouv',
-    version='0.0.2',
+    version='0.0.3',
     url='https://github.com/feddelegrand7/pygouv',
     author='Mohamed El Fodil Ihaddaden',
     author_email='ihaddaden.fodeil@gmail.com',
